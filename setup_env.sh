f635d3fa66a7db91523f550fd0758f19592b4f4b
Vicente Cortes <vicenteicc2008@gmail.com>
2023-12-22 17:00:54 -0300

    remvoe aarch64 leftover

--- setup_env.sh
+++ setup_env.sh
@@ -62,5 +62,4 @@ fi
 python3 -m pip install -r pip-requirements.txt||_error "\nFailed to install Pip Packages!\n"
 
 export CLANG38_BIN=/usr/lib/llvm-38/bin/
-export CLANG38_AARCH64_PREFIX=aarch64-linux-gnu-
 export CLANG38_ARM_PREFIX=arm-linux-gnueabihf-
