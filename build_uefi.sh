a76ea5a7adf42602adfc22fbf3039b7bd543d7f8
Vicente Cortes <vicenteicc2008@gmail.com>
2023-12-22 17:12:46 -0300

    refactor build script

--- build_uefi.sh
+++ build_uefi.sh
@@ -2,18 +2,16 @@
 
 # Function to display Help Message
 function _help(){
-	echo "Usage: ./build_uefi.sh -d <Codename> [-t <Tool Chain> -r <Build Mode> -m <RAM Size>]"
+	echo "Usage: ./build_uefi.sh -d <Codename> [-r <Build Mode>]"
 	echo
-	echo "Build Project Mu UEFI for old Qualcomm Snapdragon Platforms."
+	echo "Build Project Mu UEFI for Apple Devices."
 	echo
 	echo "Options:"
 	echo "	--device <Codename>, -d <Codename>:         Build a Device."
 	echo "	--release <Build Mode>, -r <Build Mode>:    Release mode for building, 'RELEASE' is the default or use 'DEBUG' alternatively."
-	echo "	--tool-chain <Tool Chain>, -t <Tool Chain>: Define wich Tool Chain to use, 'CLANG38' is default."
 	echo "	--help, -h:                                 Shows this Help."
-	echo "	--memory <RAM Size>, -m <RAM Size>:         Define how much Memory your Device has."
 	echo
-	echo "MainPage: https://github.com/vicenteicc2008/Mu-Msm"
+	echo "MainPage: https://github.com/sonic011gamer/Mu-LegacyApple"
 	exit 1
 }
 
@@ -22,20 +20,16 @@ function _error(){ echo -e "\033[1;31m${@}\033[0m" >&2;exit 1; }
 function _warn(){ echo -e "\033[0;33m${@}\033[0m" >&2; }
 
 # Set Default Defines
-TARGET_BUILD_MODE="DEBUG"
-MULTIPLE_RAM_SIZE="FALSE"
-TOOL_CHAIN_TAG="CLANG38"
+TARGET_BUILD_MODE="RELEASE"
 
 # Check if any args were given
-OPTS="$(getopt -o d:hfbcACDO:r:t:m:a: -l device:,help,release:,tool-chain:,memory: -n 'build_uefi.sh' -- "$@")"||exit 1
+OPTS="$(getopt -o d:hfabcACDO:r: -l device:,help,release:: -n 'build_uefi.sh' -- "$@")"||exit 1
 eval set -- "${OPTS}"
 while true
 do	case "${1}" in
 		-d|--device) TARGET_DEVICE="${2}";shift 2;;
 		-h|--help) _help 0;shift;;
 		-r|--release) TARGET_BUILD_MODE="${2}";shift 2;;
-		-t|--tool-chain) TOOL_CHAIN_TAG="${2}";shift 2;;
-		-m|--memory) TARGET_RAM_SIZE="${2}";shift 2;;
 		--) shift;break;;
 		*) _help 1;;
 	esac
@@ -58,43 +52,36 @@ then source "config/${TARGET_DEVICE}.conf"
 else _error "\nDevice configuration not found!\nCheck if your .conf File is in the 'config' Folder\n"
 fi
 
-# Check if Device has more that one Static RAM Size
-if [ -z ${TARGET_RAM_SIZE} ]; then
-	if [ ${MULTIPLE_RAM_SIZE} == TRUE ]
-	then _warn "\nThe Device you chose has more than one RAM Size!\nUse -m or --memory to define how much RAM your Device has.\n" && exit 0
-	fi
-fi
-
 # Delete Output Files if present
 rm ./BootShim/BootShim.bin &> /dev/null
 rm ./BootShim/BootShim.elf &> /dev/null
-rm ./ImageResources/bootpayload.bin &> /dev/null
-rm Mu-${TARGET_DEVICE}.img &> /dev/null
+rm Mu-${TARGET_DEVICE}.bin &> /dev/null
 
 # Compile BootShim
 cd BootShim
 make UEFI_BASE=${TARGET_FD_BASE} UEFI_SIZE=${TARGET_FD_SIZE}||_error "\nFailed to Compile BootShim!\n"
-cd ../..
-
-# Start the Real Build of the UEFI
-stuart_setup -c "Platforms/${TARGET_DEVICE_VENDOR}/${TARGET_DEVICE}Pkg/PlatformBuild.py" "TOOL_CHAIN_TAG=${TOOL_CHAIN_TAG}"||_error "\nFailed to Setup UEFI Env!\n"
-stuart_update -c "Platforms/${TARGET_DEVICE_VENDOR}/${TARGET_DEVICE}Pkg/PlatformBuild.py" "TOOL_CHAIN_TAG=${TOOL_CHAIN_TAG}"||_error "\nFailed to Update UEFI Env!\n"
-stuart_build -c "Platforms/${TARGET_DEVICE_VENDOR}/${TARGET_DEVICE}Pkg/PlatformBuild.py" "TOOL_CHAIN_TAG=${TOOL_CHAIN_TAG}" "TARGET=${_TARGET_BUILD_MODE}" "RAM_SIZE=${TARGET_RAM_SIZE}" "FD_BASE=${TARGET_FD_BASE}" "FD_SIZE=${TARGET_FD_SIZE}" "FD_BLOCKS=${TARGET_FD_BLOCKS}"||_error "\nFailed to Compile UEFI!\n"
-
-# Apply Project Mu Patches
-cp ./Patches/Compile.patch ./Mu_Basecore/
-cd Mu_Basecore
-git apply Compile.patch
 cd ..
 
-# Execute Device Specific Boot Image Creation
+# Setup and Update UEFI workspace
+stuart_setup -c "Platforms/${TARGET_DEVICE_VENDOR}/${TARGET_DEVICE}Pkg/PlatformBuild.py" TOOL_CHAIN_TAG=CLANG38||_error "\nFailed to Setup UEFI Env!\n"
+stuart_update -c "Platforms/${TARGET_DEVICE_VENDOR}/${TARGET_DEVICE}Pkg/PlatformBuild.py" TOOL_CHAIN_TAG=CLANG38||_error "\nFailed to Update UEFI Env!\n"
+stuart_build -c "Platforms/${TARGET_DEVICE_VENDOR}/${TARGET_DEVICE}Pkg/PlatformBuild.py" TOOL_CHAIN_TAG=CLANG38 "TARGET=${_TARGET_BUILD_MODE}" "FD_BASE=${TARGET_FD_BASE}" "FD_SIZE=${TARGET_FD_SIZE}" "FD_BLOCKS=${TARGET_FD_BLOCKS}"||_error "\nFailed to Compile UEFI!\n"
+
 if [ -f "config/${TARGET_DEVICE}.sh" ]
-then source configs/${TARGET_DEVICE}.sh
+then source config/${TARGET_DEVICE}.sh
 else _warn "\nImage Creation Script of ${TARGET_DEVICE} has not been Found!\nNo Boot Image Was Generated.\n"
+fi
+
+if [[ ${STATUS} != "STABLE" ]]; then
+	if [[ ${STATUS} == "UNSTABLE" ]];
+	then _warn "\n${TARGET_DEVICE} is marked as Unstable.\nThings are expected to be broken.\n"
+	elif [[ ${STATUS} == "LIMITED" ]];
+	then _warn "\n${TARGET_DEVICE} is marked as Limited.\nIt's stable but does not have much functionality\n"
+	fi
 fi
 
 git fetch &> /dev/null
 UPDATE_CHECK=$(git status)
 if [[ ${UPDATE_CHECK} == *"git pull"* ]];
-then _warn "\nYou are using an old Version of Mu-Qcom.\nThis Image may be unstable.\n"
+then _warn "\nYou are using an old Version Mu-LegacyApple.\nThis Payload may be unstable.\n"
 fi
